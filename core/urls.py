d0771dfa7573a44f199c5a1e4907b8e065180e4c
saqib-devops <saqibahmad778866@gmail.com>
2023-02-16 04:34:25 -0500

    Staticfiles : css bootstrap javascript files are added

--- core/urls.py
+++ core/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('accounts/', include('src.accounts.urls')),
+    path('', include('src.website.urls')),
 
 ]
