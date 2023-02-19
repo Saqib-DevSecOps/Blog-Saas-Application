28583255da18f2e2d0622ffb2d69bbedb559522d
saqib-devops <saqibahmad778866@gmail.com>
2023-02-19 10:27:47 -0500

    Models : Comment

--- src/website/models.py
+++ src/website/models.py
@@ -21,7 +21,6 @@ class Blog(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     meta_title = models.CharField(max_length=100, help_text='Meta title contain 100 characters')
     category = models.ForeignKey(BlogCategory, on_delete=models.SET_NULL, null=True, blank=True)
-    image = models.ImageField(upload_to='blogs/')
     description = models.TextField(max_length=4000)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
@@ -33,3 +32,15 @@ class Blog(models.Model):
 
     def get_absolute_url(self):
         return reverse('website:blog-details', args=[str(self.pk)])
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='blog_comment', null=True, blank=True)
+    comments = models.CharField(max_length=500)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='comments_reply', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
