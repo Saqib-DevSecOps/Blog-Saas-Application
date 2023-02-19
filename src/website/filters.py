a0901108f3c86acf95a24c581de48d3ef83cb1cc
saqib-devops <saqibahmad778866@gmail.com>
2023-02-19 10:29:03 -0500

    Website: >> Filter added comment, blog

---
+++ src/website/filters.py
@@ -0,0 +1,27 @@
+import django_filters
+from django.db.models import Q
+from django.forms import TextInput, ModelForm
+
+from src.website.models import BlogCategory, Blog, Comment
+
+
+class BlogFilter(django_filters.FilterSet):
+    title = django_filters.CharFilter(lookup_expr='icontains', label=''
+                                      , widget=TextInput(attrs={'placeholder': 'Search Blogs here',
+                                                                'class': 'form-control rounded-pill'}),
+                                      method='blog_filter')
+
+    class Meta:
+        model = Blog
+        fields = ['title', ]
+
+    def blog_filter(self, queryset, name, value):
+        return queryset.filter(Q(title__icontains=value) | Q(meta_title__icontains=value, description__icontains=value))
+
+
+class CommentsForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = [
+            'comments'
+        ]
